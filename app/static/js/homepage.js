86469579050b393e98dd4838c74772cc9702983b
Gia Thy Le <giathy2843@gmail.com>
2023-12-05 10:55:15 -0800

    pull

--- app/static/js/homepage.js
+++ app/static/js/homepage.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var searchButton = document.getElementById("searchButton");
-
-    searchButton.addEventListener("click", function() {
-        console.log("button clicked");
+    function handleSearch() {
+        console.log("search triggered");
         var query = document.getElementById("searchInput").value;
         // Append the query to the URL as a parameter
         var url = '/search?search_expression=' + encodeURIComponent(query);
@@ -13,15 +11,27 @@ document.addEventListener("DOMContentLoaded", function() {
             },
         })
         .then((response) => response.json())
-        .then((data => {
-            if (data.result == null){
-                alert("Can't find note!")
+        .then((data) => {
+            if (data.result == null) {
+                alert("Can't find note!");
             } else {
-                window.open(data.result)
+                window.open(data.result);
             }
-        }))
+        })
         .catch((error) => {
             console.error("Error:", error);
         });
+    }
+    
+    // Event listener for the search button click
+    var searchButton = document.getElementById("searchButton");
+    searchButton.addEventListener("click", handleSearch);
+    
+    // Event listener for "Enter" key press
+    var searchInput = document.getElementById("searchInput");
+    searchInput.addEventListener("keypress", function(event) {
+        if (event.key === 'Enter') {
+            handleSearch();
+        }
     });
 });
